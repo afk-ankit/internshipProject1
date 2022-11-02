3fd65c4140b0c10db0a102833d7701b801854613
Ankit Sharma <ankitsharmagh093@gmail.com>
2022-11-02 13:51:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useTheme } from './Store/ThemeProvider';
 
 function App() {
+  const [themeStyle, dispatch] = useTheme()
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${themeStyle}`} >
+      <h1>HERO</h1>
+      <button onClick={() => {
+        dispatch()
+      }}>Theme Toggler</button>
     </div>
   );
-}
 
+}
 export default App;
---
+++ src/Store/ThemeProvider.js
@@ -0,0 +1,26 @@
+import { createContext, useContext, useReducer } from "react"
+
+const themeContext = createContext()
+
+const initialState = "light"
+
+const reducer = (state) => {
+    if (state == 'light') {
+        return 'dark'
+    }
+    else return 'light'
+}
+
+
+
+function ThemeProvider({ children }) {
+    return (
+        <themeContext.Provider value={useReducer(reducer, initialState)}>
+            {children}
+        </themeContext.Provider>
+    )
+}
+
+
+export const useTheme = () => useContext(themeContext)
+export default ThemeProvider    
